6bb7c4988699d537dd30c284c7a9f7563f80cee8
gamaliel <fabiogamaliel@gmail.com>
2017-07-22 19:24:30 -0400

    Header component created

---
+++ client/src/app/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+import Menu from '../Menu'
+
+class Header extends Component {
+	render() {
+		return <div className='header'>
+			<div className='header__container__content'>
+				<p className='header__description'>Somos un grupo de estudiantes del Tercer curso del Bachillerato Técnico en Informática. Esta página esta enfocada en brindar información acerca del Bachillerato Técnico en informática del Centro Regional de Educación Gral. Patricio Escobar, como tambien para motivar a los alumnos interesados en este énfasis, para una buena elección y tener una noción de lo que implica este Bachillerato Técnico.</p>
+			</div>
+		</div>
+	}
+}
+
+export default Header
