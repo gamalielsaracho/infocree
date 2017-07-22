1c8a45b18545bdff74bce68eb429e8675f19c931
gamaliel <fabiogamaliel@gmail.com>
2017-07-22 12:53:35 -0400

    Menu component

--- client/src/app/components/Menu/Menu.js
+++ client/src/app/components/Menu/Menu.js
@@ -3,9 +3,34 @@ import { Link } from 'react-router'
 
 class Menu extends Component {
 	render() {
-		return <div>
-			<h4>Menu de la aplicación</h4>
-			<Link to='/eventos'>Eventos</Link>
+		return <div className='menu'>
+				<nav className='menu__btn__list'>
+					<li className='menu__btn'>
+					<Link to='/'> 
+							<span className="menu__btn_icon icon-home3"></span> Home
+					</Link>
+					</li>
+						<li className='menu__btn'>
+					<Link to='/eventos'>
+							<span className="menu__btn_icon icon-pencil2"></span> Asignaturas
+					</Link>
+						</li>
+						<li className='menu__btn'>
+					<Link to='/eventos'>
+							<span className="menu__btn_icon icon-user-check"></span> Perfil del alumno
+					</Link>
+						</li>
+						<li className='menu__btn'>
+					<Link to='/eventos'>
+							<span className="menu__btn_icon icon-question"></span> Preguntas frecuentes
+					</Link>
+						</li>
+						<li className='menu__btn'>
+					<Link to='/eventos'>
+							<span className="menu__btn_icon icon-images"></span> Promociones
+					</Link>
+						</li>
+				</nav>
 		</div>
 	}
 }
