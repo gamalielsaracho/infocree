d10002e14f0e5cea9d1da74f29309646f1fdaced
gamaliel <fabiogamaliel@gmail.com>
2017-07-23 14:59:22 -0400

    component and page QuestionList created

---
+++ client/src/question/components/QuestionList/QuestionList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class QuestionList extends Component {
+	render() {
+		return <div className='question-list'>
+			<h1 className='question-list__title'>Preguntas</h1>
+		</div>
+	}
+}
+
+export default QuestionList
---
+++ client/src/question/pages/QuestionListPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+import QuestionList from '../components/QuestionList'
+
+class QuestionListPage extends Component {
+	render() {
+		return <QuestionList/>
+	}
+}
+
+export default QuestionListPage
