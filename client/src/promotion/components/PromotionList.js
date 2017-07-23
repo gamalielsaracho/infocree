63a0acedbee3b80f0cf1e22c05b8ecb4fd7a70ec
gamaliel <fabiogamaliel@gmail.com>
2017-07-23 14:58:15 -0400

    Promotion component created

---
+++ client/src/promotion/components/PromotionList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class PromotionList extends Component {
+	render() {
+		return <div className='promotion-list'>
+			<h1 className='promotion-list__title'>Promociones</h1>
+		</div>
+	}
+}
+
+export default PromotionList
