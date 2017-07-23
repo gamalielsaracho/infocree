5fcc6b728b29b65324339c4fdb4e4c20f2d5a65b
gamaliel <fabiogamaliel@gmail.com>
2017-07-23 14:57:37 -0400

    pages and urls created

--- client/src/routes.js
+++ client/src/routes.js
@@ -3,11 +3,19 @@ import { Route, IndexRoute } from 'react-router'
 
 import AppContainer from './app/components/App'
 
-import EventListPage from './event/pages/EventListPage'
-
+// Pages.
+import HomePage from './app/pages/HomePage'
+import SubjectListPage from './subject/pages/SubjectListPage'
+import StudentProfileListPage from './student-profile/pages/StudentProfileListPage'
+import QuestionListPage from './question/pages/QuestionListPage'
+import PromotionListPage from './promotion/pages/PromotionListPage'
 
 export default (
 	<Route path='/' component={AppContainer}>
-		<Route path='/eventos' component={EventListPage}/>
+		<IndexRoute component={SubjectListPage}/>
+		<Route path='asignaturas' component={SubjectListPage}/>
+		<Route path='perfil-alumno' component={StudentProfileListPage}/>
+		<Route path='preguntas-frecuentes' component={QuestionListPage}/>
+		<Route path='promociones-anteriores' component={PromotionListPage}/>
 	</Route>
 )
