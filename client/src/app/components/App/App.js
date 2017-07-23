c1d7afe0d91a6152553e814062a67cb51f34b032
gamaliel <fabiogamaliel@gmail.com>
2017-07-23 14:55:19 -0400

    Header and Team removed from App

--- client/src/app/components/App/App.js
+++ client/src/app/components/App/App.js
@@ -1,16 +1,11 @@
 import React, { Component } from 'react'
 
 import Menu from '../Menu'
-import Header from '../Header'
-import Team from '../Team'
 
 class App extends Component {
 	render() {
 		return <div className='app'>
 			<Menu/>
-			<Header/>
-			<Team/>
-			
 			{ this.props.children }
 		</div>
 	}
