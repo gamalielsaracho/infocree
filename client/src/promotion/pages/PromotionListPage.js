424da5df167b449dba08746236c5e2833883098c
gamaliel <fabiogamaliel@gmail.com>
2017-07-23 14:58:39 -0400

    PromotionListPage component created

---
+++ client/src/promotion/pages/PromotionListPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+import PromotionList from '../components/PromotionList'
+
+class PromotionListPage extends Component {
+	render() {
+		return <PromotionList/>
+	}
+}
+
+export default PromotionListPage
