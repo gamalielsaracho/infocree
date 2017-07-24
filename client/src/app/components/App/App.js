a72370fbaf33312ffdf7207716e941898a91b81f
gamaliel <fabiogamaliel@gmail.com>
2017-07-24 13:30:19 -0400

    Footer component called in App

--- client/src/app/components/App/App.js
+++ client/src/app/components/App/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 
 import Menu from '../Menu'
+import Footer from '../Footer'
 
 class App extends Component {
 	render() {
 		return <div className='app'>
 			<Menu/>
 			{ this.props.children }
+			<Footer/>
 		</div>
 	}
 }
