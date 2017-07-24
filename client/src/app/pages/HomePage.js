3f7f9373054162da62339e250a8c24bc1f90b871
gamaliel <fabiogamaliel@gmail.com>
2017-07-24 14:13:07 -0400

    WatchVideo component added on HomePage

--- client/src/app/pages/HomePage.js
+++ client/src/app/pages/HomePage.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react'
 
 import Header from '../components/Header'
 import Team from '../components/Team'
+import WatchVideo from '../components/WatchVideo'
 
 class HomePage extends Component {
 	render() {
 		return <div>
 			<Header/>
 			<Team/>
+			<WatchVideo/>
 		</div>
 	}
 }
