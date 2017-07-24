1fdff57af6ddc598eb4caea0a98e544eb4036cbb
gamaliel <fabiogamaliel@gmail.com>
2017-07-24 08:08:07 -0400

    PromotionList created

--- client/src/promotion/components/PromotionList.js
+++ client/src/promotion/components/PromotionList.js
@@ -4,6 +4,44 @@ class PromotionList extends Component {
 	render() {
 		return <div className='promotion-list'>
 			<h1 className='promotion-list__title'>Promociones</h1>
+			<div className='promotion-list__container__courses'>
+				<div className='promotion-list__course'>
+					<div className='promotion-list__container__image'>
+						<img className='promotion-list__image' src='http://localhost:8080/images/mahaio.jpg'/>
+					</div>
+					<h4 className='promotion-list__course__title'>2014 MAHAIO</h4>
+				</div>
+				<div className='promotion-list__course'>
+					<div className='promotion-list__container__image'>
+						<img className='promotion-list__image' src='http://localhost:8080/images/kala.jpg'/>
+					</div>
+					<h4 className='promotion-list__course__title'>2013 KALAKAUA</h4>
+				</div>
+				<div className='promotion-list__course'>
+					<div className='promotion-list__container__image'>
+						<img className='promotion-list__image' src='http://localhost:8080/images/sau.jpg'/>
+					</div>
+					<h4 className='promotion-list__course__title'>2012 SAUKIRIA</h4>
+				</div>
+				<div className='promotion-list__course'>
+					<div className='promotion-list__container__image'>
+						<img className='promotion-list__image' src='http://localhost:8080/images/blu.jpg'/>
+					</div>
+					<h4 className='promotion-list__course__title'>2010 BLUETOOTH</h4>
+				</div>
+				<div className='promotion-list__course'>
+					<div className='promotion-list__container__image'>
+						<img className='promotion-list__image' src='http://localhost:8080/images/daiki.jpg'/>
+					</div>
+					<h4 className='promotion-list__course__title'>2009 DAIKIRI</h4>
+				</div>
+				<div className='promotion-list__course'>
+					<div className='promotion-list__container__image'>
+						<img className='promotion-list__image' src='http://localhost:8080/images/anagus.jpg'/>
+					</div>
+					<h4 className='promotion-list__course__title'>1997 ANAGUS</h4>
+				</div>
+			</div>
 		</div>
 	}
 }
