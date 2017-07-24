9bdcd5307843522af28a8604162ee8b7bb1c1967
gamaliel <fabiogamaliel@gmail.com>
2017-07-24 14:13:53 -0400

    bundle files

--- client/src/bundle.js
+++ client/src/bundle.js
@@ -29867,6 +29867,10 @@
 
 	var _Team2 = _interopRequireDefault(_Team);
 
+	var _WatchVideo = __webpack_require__(304);
+
+	var _WatchVideo2 = _interopRequireDefault(_WatchVideo);
+
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -29891,7 +29895,8 @@
 					'div',
 					null,
 					_react2.default.createElement(_Header2.default, null),
-					_react2.default.createElement(_Team2.default, null)
+					_react2.default.createElement(_Team2.default, null),
+					_react2.default.createElement(_WatchVideo2.default, null)
 				);
 			}
 		}]);
@@ -31469,6 +31474,73 @@
 	}(_react.Component);
 
 	exports.default = Footer;
+
+/***/ }),
+/* 304 */
+/***/ (function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+
+	var _WatchVideo = __webpack_require__(305);
+
+	var _WatchVideo2 = _interopRequireDefault(_WatchVideo);
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+	exports.default = _WatchVideo2.default;
+
+/***/ }),
+/* 305 */
+/***/ (function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	Object.defineProperty(exports, "__esModule", {
+		value: true
+	});
+
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+	var _react = __webpack_require__(1);
+
+	var _react2 = _interopRequireDefault(_react);
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+	var WatchVideo = function (_Component) {
+		_inherits(WatchVideo, _Component);
+
+		function WatchVideo() {
+			_classCallCheck(this, WatchVideo);
+
+			return _possibleConstructorReturn(this, (WatchVideo.__proto__ || Object.getPrototypeOf(WatchVideo)).apply(this, arguments));
+		}
+
+		_createClass(WatchVideo, [{
+			key: 'render',
+			value: function render() {
+				return _react2.default.createElement(
+					'div',
+					{ className: 'watch-video' },
+					_react2.default.createElement('iframe', { className: 'watch-video__play', src: 'https://www.youtube.com/embed/8lp20JFiB4s' })
+				);
+			}
+		}]);
+
+		return WatchVideo;
+	}(_react.Component);
+
+	exports.default = WatchVideo;
 
 /***/ })
 /******/ ]);
